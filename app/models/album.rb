d3feec8755a9c23656faffa9070eb080e07a0563
Joshua Book <joshua.book@gmail.com>
2016-02-17 18:38:23 -0500

    add relationship between artist on album

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,6 @@ class Album < ActiveRecord::Base
   validates :title, uniqueness: {scope: :artist_id}
   validates :title, presence: true
 
-  has_many :songs
+  has_many   :songs
+  belongs_to :artist
 end
