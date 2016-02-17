90780144ecb50a76be4339b38a87577da10abbc8
Joshua Book <joshua.book@gmail.com>
2016-02-17 18:42:32 -0500

    set artist on album creation

--- app/services/collection_service.rb
+++ app/services/collection_service.rb
@@ -2,7 +2,7 @@ class CollectionService
   class << self
     def add_song hash
       artist = Artist.find_or_create_by(name: hash[:artist])
-      album = Album.find_or_create_by(title: hash[:album])
+      album = Album.find_or_create_by(title: hash[:album], artist: artist)
       Song.create(title: hash[:song], album: album)
     end
   end
