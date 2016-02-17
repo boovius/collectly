4eaace7462a1cc1395b0956cff9856aa733fae84
Joshua Book <joshua.book@gmail.com>
2016-02-17 18:38:04 -0500

    update artist attr usage to name from title

--- app/services/collection_service.rb
+++ app/services/collection_service.rb
@@ -1,6 +1,7 @@
 class CollectionService
   class << self
     def add_song hash
+      artist = Artist.find_or_create_by(name: hash[:artist])
       album = Album.find_or_create_by(title: hash[:album])
       Song.create(title: hash[:song], album: album)
     end
