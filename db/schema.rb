97d3b900b0d8409f295c72978d1078ff7aa3c43f
Joshua Book <joshua.book@gmail.com>
2016-02-17 18:25:10 -0500

    add artists table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206174902) do
+ActiveRecord::Schema.define(version: 20160217232410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160206174902) do
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
