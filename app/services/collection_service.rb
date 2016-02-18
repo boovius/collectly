4037e1f0ddc41dea27553f97c0aef8f82a166c74
Joshua Book <joshua.book@gmail.com>
2016-02-17 19:45:29 -0500

    downcase artists coming in

--- app/services/collection_service.rb
+++ app/services/collection_service.rb
@@ -1,7 +1,7 @@
 class CollectionService
   class << self
     def add_song hash
-      artist = Artist.find_or_create_by(name: hash[:artist])
+      artist = Artist.find_or_create_by(name: hash[:artist].downcase)
       album = Album.find_or_create_by(title: hash[:album], artist: artist)
       Song.create(title: hash[:song], album: album)
     end
