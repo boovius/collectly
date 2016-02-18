3065aabe4e3f971ed903fbcf676f1f311d85d598
Joshua Book <joshua.book@gmail.com>
2016-02-18 13:45:49 -0500

    update collection service

--- app/services/collection_service.rb
+++ app/services/collection_service.rb
@@ -2,7 +2,7 @@ class CollectionService
   class << self
     def add_song hash
       artist = Artist.find_or_create_by(name: hash[:artist].downcase)
-      album = Album.find_or_create_by(title: hash[:album], artist: artist)
+      album = Album.find_or_create_by(title: hash[:album], image_url: hash[:album_image_url], artist: artist)
       song = Song.new(title: hash[:song], album: album)
       song.save
     end
