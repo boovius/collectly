a99c6f3c41f49225601576928989d18f2dc49269
Joshua Book <joshua.book@gmail.com>
2016-02-17 19:46:02 -0500

    add validation and relationships to artist and song models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ActiveRecord::Base
+  validates :name, uniqueness: true
+  validates :name, presence: true
+
+  has_many :albums
+  has_many :songs, through: :albums
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,8 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: {scope: :album_id}
 
   belongs_to :album
+
+  def artist
+    album.artist
+  end
 end
