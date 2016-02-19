edbf19f8b2247cb4a178f7eaa6b4c530f148b83e
Joshua Book <joshua.book@gmail.com>
2016-02-18 19:19:24 -0500

    add favorite columns to song and artist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218234128) do
+ActiveRecord::Schema.define(version: 20160219001832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160218234128) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "favorite"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160218234128) do
     t.integer  "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "favorite"
   end
 
   create_table "users", force: :cascade do |t|
