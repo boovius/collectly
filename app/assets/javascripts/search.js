c000d6928bd3ae9098ae45ef6f67aefe0d9e9f69
Joshua Book <joshua.book@gmail.com>
2016-02-19 13:09:27 -0500

    remove stale results

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   $('#query-submit').click(function(){
     var query = $('#query').val();
+    var resultsContainer = $('.search-results');
+    removeStaleResults(resultsContainer);
 
     $.get('/search', {query: query}, function(data){
-      var resultsContainer = $('.search-results');
-
       var dataKeys = Object.keys(data);
 
+      var haveResults = false;
       for(var i = 0; i < dataKeys.length; i++){
         var results = data[dataKeys[i]];
+        results.length > 0 || haveResults === true ? haveResults = true : haveResults = false
+
         var mediaContainer = resultsContainer.find('.'+dataKeys[i]).find('ul')
 
         if (dataKeys[i] === 'artists') {
@@ -23,6 +26,14 @@ $(document).ready(function(){
           $(mediaContainer).append(html);
         }
       }
+      if (!haveResults) {
+        $(resultsContainer.parent()).append("<h4 id='no-results'>No Results</h4>");
+      }
     });
   });
 });
+
+function removeStaleResults(container) {
+  container.find('li').remove()
+  container.parent().find('#no-results').remove();
+}
