a9b3054937df514850575047c3c06d53fd0de0da
Joshua Book <joshua.book@gmail.com>
2016-03-07 16:12:20 -0500

    defactor search js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,29 +8,39 @@ $(document).ready(function(){
     removeStaleResults(resultsContainer);
 
     $.get('/search', {query: query}, function(data){
-      var dataKeys = Object.keys(data);
 
       var haveResults = false;
-      for(var i = 0; i < dataKeys.length; i++){
-        var results = data[dataKeys[i]];
-        results.length > 0 || haveResults === true ? haveResults = true : haveResults = false
-
-        var mediaContainer = resultsContainer.find('.'+dataKeys[i]).find('ul')
-
-        if (dataKeys[i] === 'artists') {
-          var accessor = 'name';
-        } else {
-          var accessor = 'title';
-        }
-
-        for(var j = 0; j<results.length;j++){
-          var html = '<li><a href=/' + dataKeys[i] + '/' + results[j].id;
-          html += '>' +results[j][accessor]+'</a></li>';
-          $(mediaContainer).append(html);
-        }
-      }
+      data.artists.length > 0 || haveResults === true ? haveResults = true : haveResults = false
+      data.albums.length > 0 || haveResults === true ? haveResults = true : haveResults = false
+      data.songs.length > 0 || haveResults === true ? haveResults = true : haveResults = false
       if (!haveResults) {
         $(resultsContainer.parent()).append("<h4 id='no-results'>No Results</h4>");
+        return;
+      }
+
+      for(var i = 0; i < data.artists.length; i++){
+        var mediaContainer = resultsContainer.find('.artists').find('ul')
+
+        var html = '<li><a href=/artists/' + data.artists[i].id;
+        html += '>' + data.artists[i].name + '</a></li>';
+
+        $(mediaContainer).append(html);
+      }
+      for(var i = 0; i < data.albums.length; i++){
+        var mediaContainer = resultsContainer.find('.albums').find('ul')
+
+        var html = '<li><a href=/albums/' + data.albums[i].id;
+        html += '>' + data.albums[i].title + '</a></li>';
+
+        $(mediaContainer).append(html);
+      }
+      for(var i = 0; i < data.songs.length; i++){
+        var mediaContainer = resultsContainer.find('.songs').find('ul')
+
+        var html = '<li><a href=/songs/' + data.songs[i].id;
+        html += '>' + data.songs[i].title + '</a></li>';
+
+        $(mediaContainer).append(html);
       }
     });
   });
