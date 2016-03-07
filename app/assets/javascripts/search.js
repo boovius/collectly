58c47017961a28d8354cf2d68182a5ada7080fe0
Joshua Book <joshua.book@gmail.com>
2016-03-07 15:19:18 -0500

    no results message

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,6 @@
 search = angular.module('search', []);
 
 search.controller('SearchCtrl', function($scope){
-
   $('#query-submit').click(function(e){
     e.preventDefault();
     $('.search-results div').show();
@@ -13,9 +12,25 @@ search.controller('SearchCtrl', function($scope){
       $scope.albums = data.albums;
       $scope.songs = data.songs;
 
+      $scope.noResults = hasNoResults(data);
+
       $scope.$apply();
     });
   });
 });
 
+function hasNoResults(data){
+  return noArtists(data) && noAlbums(data) && noSongs(data);
+}
 
+function noArtists(data){
+  return data.artists.length === 0;
+}
+
+function noAlbums(data){
+  return data.albums.length === 0;
+}
+
+function noSongs(data){
+  return data.songs.length === 0;
+}
