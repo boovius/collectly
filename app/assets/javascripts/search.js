822e37b7ddf907e78b5d57cfc6a5255b6e59de7d
Joshua Book <joshua.book@gmail.com>
2016-03-07 14:50:01 -0500

    angularize search results

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,44 +1,21 @@
 search = angular.module('search', []);
 
-$(document).ready(function(){
+search.controller('SearchCtrl', function($scope){
+
   $('#query-submit').click(function(e){
     e.preventDefault();
     $('.search-results div').show();
 
     var query = $('#query').val();
-    var resultsContainer = $('.search-results');
-    removeStaleResults(resultsContainer);
 
     $.get('/search', {query: query}, function(data){
-      var dataKeys = Object.keys(data);
+      $scope.artists = data.artists;
+      $scope.albums = data.albums;
+      $scope.songs = data.songs;
 
-      var haveResults = false;
-      for(var i = 0; i < dataKeys.length; i++){
-        var results = data[dataKeys[i]];
-        results.length > 0 || haveResults === true ? haveResults = true : haveResults = false
-
-        var mediaContainer = resultsContainer.find('.'+dataKeys[i]).find('ul')
-
-        if (dataKeys[i] === 'artists') {
-          var accessor = 'name';
-        } else {
-          var accessor = 'title';
-        }
-
-        for(var j = 0; j<results.length;j++){
-          var html = '<li><a href=/' + dataKeys[i] + '/' + results[j].id;
-          html += '>' +results[j][accessor]+'</a></li>';
-          $(mediaContainer).append(html);
-        }
-      }
-      if (!haveResults) {
-        $(resultsContainer.parent()).append("<h4 id='no-results'>No Results</h4>");
-      }
+      $scope.$apply();
     });
   });
 });
 
-function removeStaleResults(container) {
-  container.find('li').remove()
-  container.parent().find('#no-results').remove();
-}
+
